33ed979547b969b26747ffd5643a9fb543a40f99
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-09-13 13:29:52 +0200

    added global style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+import SoundPlayer from './components/SoundPlayer/';
+// import { ThemeProvider } from 'styled-components';
+// import Theme from './styles/Theme';
+// import './App.css';
 
-function App() {
+import playlists from './data/playlists.json';
+import youtube from './data/youtube.json';
+import tracks from './data/tracks.json';
+
+const data = [playlists, youtube, tracks].flat();
+
+const App = () => {
   return (
-    <div>
-      
+    <div className="App">
+      <GlobalStyle />
+      {/* <ThemeProvider theme={Theme} > */}
+      <></>
+      {/* </ThemeProvider> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+const GlobalStyle = createGlobalStyle`
+    *,*::before,*::after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+`;
+
+export default GlobalStyle;
